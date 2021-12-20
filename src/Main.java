93945dbb31a252c7efb3ff47d3f1dd7474d4745f
Hyun Juwon <e215760@ie.u-ryukyu.ac.jp>
2021-12-20 21:18:17 +0900

    Step3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     
     public static void main(String[] args){
+        /*
         try{
             String str = null;
             System.out.println(str.length());
@@ -8,6 +9,16 @@ public class Main {
             System.out.println("条件2 : NullPointerExceptionが発生した!");
             System.out.println("条件3 : " +e.getMessage());
         }
+        */
+        //String str = "百二十三";
+        //Integer.parseInt(str);
+        String str = "123";
+        //Integer.parseInt(str);
+        System.out.println(str.getClass().getSimpleName());
+        System.out.println(Integer.parseInt(str));
+        
+        
+
     }
 
 }
